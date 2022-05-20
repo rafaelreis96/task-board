fe7cbc354bfddc6292043bfbe3ccf4a4f50e0099
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2022-05-19 20:04:27 -0400

    feat: criação do shared modules e botao novo

---
+++ src/app/shared/components/button-novo/button-novo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-novo',
+  templateUrl: './button-novo.component.html',
+  styleUrls: ['./button-novo.component.scss']
+})
+export class ButtonNovoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
