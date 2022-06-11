a805c0a3d53d6a66ff78aa43169b0da841d629de
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2022-06-11 19:55:40 -0400

    feat: Adicionando  provider para salvar dados no localstorage

---
+++ src/app/board/column/column.service.ts
@@ -0,0 +1,19 @@
+import { Column } from './column.model';
+import { Injectable } from '@angular/core';
+import { AbstractDao } from '@app/core/services/abstract.dao';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColumnService extends AbstractDao<Column>{
+
+  constructor() {
+    super('COLUMN');
+  }
+
+  override create(t: Column) : void {
+    t.id = Math.floor(Math.random() * 1000000);
+    super.create(t);
+  }
+
+}
