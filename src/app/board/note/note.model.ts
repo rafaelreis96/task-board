5effc7b82f7bba3d3ad4cad4fb090b18fb78806d
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2022-06-11 20:03:37 -0400

    feat: Adicionando funcionalidade de salvar dados no localstorage

--- src/app/board/note/note.model.ts
+++ src/app/board/note/note.model.ts
@@ -1,13 +1,12 @@
+import { Periodo } from './periodo.model';
 import { Column } from './../column/column.model';
 
 export interface Note {
+  id: number,
   titulo: string;
   descricao: string;
-  coluna: Column,
-  datas: {
-    inicio: Date;
-    termino: Date;
-  }
+  periodo: Periodo;
+  coluna?: Column;
   data_modificacao: Date;
   data_criacao: Date;
 }
